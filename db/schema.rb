4f1add553004f2b4e39eb007bfc775b1c80873a3
jonathanpberger <jonathanpberger@gmail.com>
2010-06-06 13:45:07 -0400

    2.3.5 finished microposting demo app

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100606165154) do
+ActiveRecord::Schema.define(:version => 20100606173722) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
-    t.integer  "used_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
